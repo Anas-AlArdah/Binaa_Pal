72bcf5782df32ac75720f9940ae3125cf9d6f7d2
Anas_AlArdah <anas.alardah.ar@gmail.com>
2026-03-28 20:02:50 +0200

    packages edits

--- client/src/index.js
+++ client/src/index.js
@@ -12,7 +12,7 @@ import Crafts from "./pages/CraftsmanProfile/Crafts";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Homepage />
+    <App />
   </React.StrictMode>
 );
 
--- client/src/pages/CraftsmanProfile/Crafts.jsx
+++ client/src/pages/CraftsmanProfile/Crafts.jsx
@@ -107,6 +107,7 @@ function Crafts() {
           </div>
         </div>
       </div>
+      
     </>
   );
 }
